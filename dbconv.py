c549938f8574d276eb61a419891c6aa3a91e8e85
BobDev94 <108867512+BobDev94@users.noreply.github.com>
2022-12-13 20:19:05 +0545

    Update dbconv.py

--- dbconv.py
+++ dbconv.py
@@ -15,55 +15,104 @@ def conv_csv(n,temp):
                 for j in range(1,len(dr)+1):
                     sheet.cell(row=i, column=j).value=dr[j-1]
                 i+=1
+            pass
         new_file=temp.split('.')[0]+'.xlsx'
         wb.save(new_file)
         print(f'The file has been ported into Excel as {new_file}')
-        cont=input('Are there any more files to convert? Enter \'y\' if yes, and just hit enter otherwise')
-        main() if cont.lower()=='y' else print('Exiting...')
+        return new_file
 
     #CONVERSION TO DATABASE SECTION
     if n=='2':
         print('Converting to Database Format(.db). Just a moment, please')
-        rel=input('Create a relational database? Hit y/Y if yes, and just hit enter if not')
 
         #TABLE CREATION SECIION
-        if not rel:
-            brr=temp.split('.')[0]
-            filename=brr+'.db' #FILENAME
-            table_name=brr.upper()+'_TABLE'
-            create_query=f'CREATE TABLE {table_name}('
-            with open(temp,'r') as csv_rf:
-                csv_data=csv.reader(csv_rf)
-                #NEED FIRST 2 ROWS FOR COLUMN NAME(1) AND TYPE(2)
-                count=0
-                rowstore=[]
-                for i in csv_data:
-                    rowstore.append(i)
-                    count+=1
-                    if count==2:
-                        break
-                count=0
-                for i in range(len(rowstore[0])):
-                    jtype=' INTEGER, ' if rowstore[1][i].isdigit()==True else ' TEXT, '
-                    create_query+=rowstore[0][i]+jtype
-                create_query=create_query.strip(', ')
-                create_query+=')'
-                conn=sqlite3.connect(filename)
-                c=conn.cursor()
-                c.execute(create_query) #TABLE IS CREATED!
+        brr=temp.split('.')[0]
+        filename=brr+'.db' #FILENAME
+        table_name=brr.upper()+'_TABLE'
+        create_query=f'CREATE TABLE {table_name}(' #AUTO CREATE TABLE NAME
+        with open(temp,'r') as csv_rf:
+            csv_data=csv.reader(csv_rf)
+            #NEED FIRST 2 ROWS FOR COLUMN NAME(1) AND TYPE(2)
+            count=0
+            rowstore=[]
+            for i in csv_data:
+                rowstore.append(i)
+                count+=1
+                if count==2:
+                    break
+            count=0
+            for i in range(len(rowstore[0])):
+                jtype=' INTEGER, ' if rowstore[1][i].isdigit()==True else ' TEXT, '
+                create_query+=rowstore[0][i]+jtype #Like Name TEXT or Age INTEGER
+            create_query=create_query.strip(', ')
+            create_query+=')'
+            conn=sqlite3.connect(filename)
+            c=conn.cursor()
+            c.execute(create_query) #TABLE IS CREATED!
+            pass
 
-            with open(temp,'r') as csv_rf:
-                csv_data=csv.reader(csv_rf)
-                for i in csv_data:
-                    if count==0:
-                        count+=1
-                        continue
-                    c.execute(f'INSERT INTO {table_name}{tuple(rowstore[0])} VALUES{tuple(i)}')
-            c.execute(f"SELECT * FROM {table_name}")
-            print(c.fetchall())
-            conn.commit()
-            conn.close()
+        with open(temp,'r') as csv_rf:
+            csv_data=csv.reader(csv_rf)
+            for i in csv_data:
+                if count==0:
+                    count+=1
+                    continue
+                c.execute(f'INSERT INTO {table_name}{tuple(rowstore[0])} VALUES{tuple(i)}')
+            pass
+
+        conn.commit()
+        conn.close()
     return filename
+
+
+def conv_txt(n,temp): #WIP
+
+    with open(temp,'r') as txt_rf:
+        rf=txt_rf.readlines()
+        clean_rf=[rf[i].strip('\n').split(',') for i in range(len(rf))]
+        pass
+
+    #CONVERSION TO EXCEL
+    if n=='1':
+        print('Converting to xlsx format(Excel). Just a moment, please.')
+        wb=xl.Workbook()
+        sheet=wb.active
+        i=1 #Row count
+        for dr in (clean_rf):
+            for j in range(1,len(dr)+1):
+                sheet.cell(row=i,column=j).value=dr[j-1]
+            i+=1
+        new_file=temp.split('.')[0]+'.xlsx'
+        wb.save(new_file)
+        print(f'The file has been ported into Excel as {new_file}')
+        return new_file
+
+    #CONVERSION TO DATABASE SECTION
+    if n=='2':
+        print('Converting to Database Format(.db). Just a moment, please')
+
+        #TABLE CREATION SECIION
+        filename=temp.split('.')[0]+'.db'
+        table_name=temp.split('.')[0].upper()+'_TABLE'
+        create_query=f'CREATE TABLE {table_name}('
+        #clean_rf is the data to be formatted into db
+        for i in range(len(clean_rf[0])):
+            dtype=' INTEGER,' if clean_rf[1][i].isdigit() else ' TEXT,'
+            create_query+=clean_rf[0][i]+dtype
+        create_query=create_query.strip(', ')
+        create_query+=')'
+        conn=sqlite3.connect(filename)
+        c=conn.cursor()
+        c.execute(create_query) #TABLE IS CREATED!
+
+        count=0
+        for i in clean_rf:
+            ins_query=f'INSERT INTO {table_name}{tuple(clean_rf[0])} VALUES {tuple(i)}'
+            c.execute(ins_query)
+        conn.commit()
+        conn.close()
+        return filename
+
 
 def conv_xlsx(temp):
     wb=xl.load_workbook(temp)
@@ -71,7 +120,8 @@ def conv_xlsx(temp):
     file=temp.split('.')[0]
     file_name=file+'.db'
     table_name=file.upper()+'_TABLE'
-
+    
+    #TABLE CREATE QUERY SECTION
     create_query=f'CREATE TABLE {table_name}('
     for i in range(1, sheet.max_column+1):
         create_query+=str(sheet.cell(row=1,column=i).value)
@@ -92,8 +142,9 @@ def conv_xlsx(temp):
     conn.close()
     return file_name 
 
+
 def db_operations(db_name):
-    print(db_name)
+
     while 1:
         print('''\nDB Operations:
 1.  View all
@@ -105,6 +156,7 @@ def db_operations(db_name):
 7.  Clear Screen
         ''')
         op=input('Enter operation number: ')
+        print('')
 
         if op=='6':
             break
@@ -126,19 +178,22 @@ def db_operations(db_name):
             print(tabulate(table, headers=head))
             continue
 
-        #WIP
         elif op=='2':
             names=tuple(head)
 
             print(f'\nKeep entering new values in the proper format:{names}, apostrophes optional. Hit enter to stop.')
             while 1:
-                new_val=input('Enter values, separated by a comma, apostrope optional:\n')
-                new_val_lis=str(tuple(new_val.split(',')))
-                if not new_val:
-                    break
-                insert_que=f'INSERT INTO {table_name}{names} VALUES{new_val_lis}'
-                c.execute(insert_que)
-                conn.commit()
+                try:
+                    new_val=input('Enter values, separated by a comma, apostrope optional:\n')
+                    new_val_lis=str(tuple(new_val.split(',')))
+                    if not new_val:
+                        break
+                    insert_que=f'INSERT INTO {table_name}{names} VALUES{new_val_lis}'
+                    c.execute(insert_que)
+                    conn.commit()
+                except:
+                    print('Data entered in an invalid format. Please enter data in the correct format')
+                    continue
             continue
 
         elif op=='3':
@@ -153,11 +208,11 @@ def db_operations(db_name):
                 if b not in [1,2,3]:
                     print('Error')
                     continue
-                cons=input('Enter comparison constant: ')
+                cons=input('Enter comparison constant: ').title()
+                print('')
 
                 filt_view=f"SELECT * FROM {table_name} WHERE {head[a-1]}{ops[b-1]}{cons}" if cons.isdigit()==True else f"SELECT * FROM {table_name} WHERE {head[a-1]}{ops[b-1]}"+f"'{cons}'"
 
-                print(filt_view)
                 c.execute(filt_view)
                 table=c.fetchall()
                 print(tabulate(table, headers=head))
@@ -171,19 +226,17 @@ def db_operations(db_name):
                     updt=input('\nEnter the values in the format: <new_assignment>,<condition>. To exit, just hit enter:\n')
                     if not updt:
                         break
-                    
                     brr=updt.split(',')
                     brr1_e,brr2_e=brr[0],brr[1]
 
                     brr1=brr[0].split('=')
                     if brr1[1].isdigit()!=True:
-                        brr1_e=brr1[0]+'='+'\''+f'{brr1[1]}'+'\''
+                        brr1_e=brr1[0].title()+'='+'\''+f'{brr1[1].title()}'+'\''
 
                     brr2=brr[1].split('=')
                     if brr2[1].isdigit()!=True:
-                        brr2_e=brr2[0]+'='+'\''+f'{brr2[1]}'+'\''
+                        brr2_e=brr2[0].title()+'='+'\''+f'{brr2[1].title()}'+'\''
 
-                    print(brr1_e, brr2_e)
                     update_table=f'UPDATE {table_name} SET {brr1_e} WHERE {brr2_e}'
                     c.execute(update_table)
                     conn.commit()
@@ -223,33 +276,44 @@ def db_operations(db_name):
         conn.commit()
         conn.close()
 
+
 def main():
     while 1:
-        #INPUT VALIDATION SECTION
         print(os.getcwd())
         print('\n Firing up Database Converter/Editor...\n')
+
+        #INPUT VALIDATION SECTION
         while 1:
-            file=input('Please enter the file or the folder path: ')
+            file=input('Please enter the file name or the folder path: ')
             if '/' in file or '\\' in file:
+
                 try:
                     os.chdir(file)
+                    #REMOVE THE NEXT THREE LINES BEFORE FINAL PRESENTATION
                     for i in os.listdir():
                         if '.db' in i:
                             os.remove(i)
                 except:
                     sys.exit('The specified path is either mistaken or does not exist')
+
                 file_list=[]
                 for i in os.listdir():
                     if '.csv' in i or '.xlsx' in i or '.db' in i or '.txt' in i:
                         file_list.append(i)
+
                 if len(file_list)==1:
                     temp=file_list[0]
+                elif len(file_list)==0:
+                    print('No files in the accepted formats present in the user specified folder path')
+                    temp=0
+                    break
                 else:
-                    print('Which file is to be edited?')
+                    print('\nWhich file is to be edited?')
                     print(*(str(i+1)+' '+file_list[i] for i in range(len(file_list))),sep='\n')
-                    choice=int(input('Enter file number: '))
+                    choice=int(input('\nEnter file number: '))
                     temp=file_list[choice-1]
                 break
+
             else:
                 if file not in os.listdir(os.getcwd()):
                     print('Specified file does not exist. Please enter valid file or filepath.')
@@ -258,10 +322,25 @@ def main():
                 break
 
         #FILE IDENTIFICATION SECTION
+        if temp==0:
+            break
+
         if '.csv' in temp:
             print('CSV file was detected.')
             file=conv_csv(input('Enter corresponding number to choose conversion type:\n1: .xlsx\n2: .db'),temp)
             print(f'{file} was generated')
+            adv=input('Is there anything else to do? Hit y/Y if yes, and just hti enter if not\n')
+            if not adv:
+                sys.exit('Exiting...')
+
+        elif '.txt' in temp: #Basically CSV's, but in a text file instead of csv format
+            print('tct file detected.')
+            file=conv_txt(input('Enter corresponding number to choose conversion type:\n1: .xlsx\n2: .db'),temp)
+            print(f'{file} was generated')
+            adv=input('Is there anything else to do? Hit y/Y if yes, and just hti enter if not\n')
+            if not adv:
+                sys.exit('Exiting...')
+
         
         elif '.xlsx' in temp:
             print('Excel file was detected. Converting to .db, please wait a moment')
@@ -269,12 +348,14 @@ def main():
             print(f'{file} was generated')
         
         if '.db' in temp:
+            print('Database format file was detected.')
             file=temp
+
         if '.db' in file:
-            db_op=input('Perform Database operation?')
+            db_op=input('Perform Database operation? Enter any value if yes, hit enter if no.\n')
             if db_op:
-                db_operations(file)
-            cont=input('Any other conversion operations to occur? Hit enter if no, y/Y if yes')
+                db_operations(file) #CALLS db_operatons and sends it the name of the file
+            cont=input('Any other conversion operations to occur? Hit enter if no, y/Y if yes: ')
             if not cont:
                 break
             if cont:
